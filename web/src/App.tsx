cbbfd6e8d20c507bfcb0ee3983132b88255e70b3
Rebeca Souza <rebeca_souza85@hotmail.com>
2020-10-13 00:09:22 -0300

    Adds assets and global style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import GlobalStyles from './assets/styles/global';
+
 function App() {
   return (
    <div className="App">
+     <GlobalStyles />
        <h1>Hello World</h1>
    </div>
   );
