e0c0aee09bd12d14f84c49a7ade81ef008da18be
Rebeca Souza <rebeca_souza85@hotmail.com>
2021-01-10 15:10:58 -0300

    Adds map marker and link to create orphanage

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
-import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import mapMarker from '../../assets/images/map-marker.svg';
 import colors from '../../assets/styles/colors';
-import Button from '../components/Button';
+import Button from '../../components/Button';
 
 import 'leaflet/dist/leaflet.css';
 
-import {
-  Container,
-  Aside,
-  Header,
-  Footer
-} from './styles';
+import { Container, Aside, Header, Footer } from './styles';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarker,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMap: React.FC = () => {
   return (
     <Container>
       <Aside>
         <Header>
-          <img src={mapMarker} alt="Happy"/>
+          <img src={mapMarker} alt="Happy" />
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças estão esperando a sua visita :)</p>
         </Header>
@@ -30,24 +34,35 @@ const OrphanagesMap: React.FC = () => {
         </Footer>
       </Aside>
 
-      <Map 
-        center={[-5.8073447,-35.2210328]}
+      <Map
+        center={[-5.8073447, -35.2210328]}
         zoom={13.14}
-        style={{ width: '100%', height: '100%'}}
-        >
+        style={{ width: '100%', height: '100%' }}>
         <TileLayer
-          // Openstret map 
-          // url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> 
+          // Openstret map
+          // url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           // Regular mapbox style
           // url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
           // Custom mapbox style
           url={`https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USER}/${process.env.REACT_APP_MAPBOX_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
-          
+
+        <Marker icon={mapIcon} position={[-5.8073447, -35.2210328]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup">
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Button 
-        to="/"
+      <Button
+        to="/orphanages/create"
         position="absolute"
         height="64px"
         width="64px"
@@ -57,9 +72,8 @@ const OrphanagesMap: React.FC = () => {
         backgroundColor={colors.blue}
         color="white"
         hoverBackgroundColor={colors.hoverDarkerBlue}
-        hoverColor="white"
-        >
-        <FiPlus size={35}/>
+        hoverColor="white">
+        <FiPlus size={35} />
       </Button>
     </Container>
   );
--- web/src/pages/OrphanagesMap/styles.ts
+++ web/src/pages/OrphanagesMap/styles.ts
@@ -5,12 +5,47 @@ import colors from '../../assets/styles/colors';
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  
+
   position: relative;
   display: flex;
-  
+
   .leaflet-container {
     z-index: 5;
+
+    .map-popup {
+      .leaflet-popup-content-wrapper {
+        background: rgba(255, 255, 255, 0.8);
+        border-radius: 20px;
+        box-shadow: none;
+      }
+
+      .leaflet-popup-content {
+        color: ${colors.titleTextBlue};
+        font-size: 20px;
+        font-weight: bold;
+        margin: 8px 12px;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        a {
+          width: 40px;
+          height: 40px;
+          background: ${colors.blue};
+          box-shadow: 14.2858px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+          border-radius: 12px;
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      }
+
+      .leaflet-popup-tip-container {
+        display: none;
+      }
+    }
   }
 
   a {
@@ -20,13 +55,16 @@ export const Container = styled.div`
 
 export const Aside = styled.aside`
   width: 440px;
-  background: linear-gradient(329.54deg, ${colors.gradientBlue} 0%, ${colors.gradientGreen} 100% );
+  background: linear-gradient(
+    329.54deg,
+    ${colors.gradientBlue} 0%,
+    ${colors.gradientGreen} 100%
+  );
   padding: 80px;
 
   display: flex;
   flex-direction: column;
-  justify-content: space-between; 
-
+  justify-content: space-between;
 `;
 
 export const Header = styled.header`
@@ -35,8 +73,8 @@ export const Header = styled.header`
     font-weight: 800;
     line-height: 42px;
     margin-top: 60px;
-  } 
-  
+  }
+
   p {
     line-height: 28px;
     margin-top: 24px;
